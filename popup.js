f686194bafdab9d26868c337d6f6ac21fd43c746
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-08-15 19:58:12 +0900

    TO DO : getAlarmTiming

--- popup.js
+++ popup.js
@@ -1,25 +1,84 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+const body = document.querySelector("body");
+const defaultText = "유정아 공부해야지";
+let speechText = defaultText;
+const userInputContainer = `
+      <div class="container">
+      <h4>현재 보고있는 영상이 끝나면 나올 텍스트 입력</h4>
+      <textarea id="text">${defaultText}</textarea>
+      <button>타이머 시작하기</button>
+    </div>
+`;
+const indicator = `
+  <div class="indicator">
+    <span>유튜브가 아닌 웹사이트</span>
+  </div>
+`;
 
-//확장 프로그램의 저장소에 저장된 값을 기반으로 팝업 버튼의 색상을 지정합니다.
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// let utterance = new SpeechSynthesisUtterance("공부해야지");
+// speechSynthesis.speak(utterance);
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//TO DO : interval clear
 
+const getAlarmTiming = () => {
+  const video = document.querySelector(".html5-main-video");
+  const duration = video.duration;
+  setInterval(() => {
+    const currentTime = video.currentTime;
+    const ad = document.querySelector(".ytp-ad-text");
+    console.log(currentTime, duration);
+    //TO DO : 50%, 70%, 80%, 85%, 90%, 95%, 100%를 구해서 이 때마다 speech API 호출
+    //조건 : !ad
+  }, 1000);
+};
+
+const getCurrentTab = async () => {
+  let [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: getAlarmTiming,
   });
-});
-//이벤트 또는 특정 경우에 응답하여 실행해야 하는 콘텐츠 스크립트에 프로그래밍 방식 삽입을 사용합니다.
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+};
+
+const getUserText = () => {
+  const textarea = document.querySelector("textarea");
+  textarea.addEventListener("change", (event) => {
+    const userText = event.target.value;
+    chrome.storage.sync.set({ storageText: userText });
+    speechText = userText;
   });
-}
+  getCurrentTab();
+};
+
+const getStorageText = () => {
+  const textarea = document.querySelector("textarea");
+  chrome.storage.sync.get("storageText", ({ storageText }) => {
+    if (storageText) {
+      textarea.value = storageText;
+      speechText = storageText;
+    } else {
+      textarea.value = defaultText;
+    }
+    getUserText();
+  });
+};
+
+const getCurrentTapURL = async () => {
+  await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    let url = tabs[0].url;
+    const currentUrl = url.split("/")[2];
+    if (currentUrl === "www.youtube.com") {
+      body.innerHTML = userInputContainer;
+      getStorageText();
+    } else {
+      body.innerHTML = indicator;
+    }
+  });
+};
+
+const init = () => {
+  getCurrentTapURL();
+};
+init();
